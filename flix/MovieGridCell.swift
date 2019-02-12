212c837665af498c9d49463c46c0f14dc40fa600
Geoffrey Montague <gmontague@csumb.edu>
2019-02-11 23:39:25 -0800

    Added superhero grid and collection layout. Finished Flix Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by geoff on 2/11/19.
+//  Copyright © 2019 geoffmontague. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
